1acd0b61735cf2e242ad2bfb6951ce7d17be1d1c
Paulo Almeida <eu@paulo.digital>
2024-02-02 09:25:02 -0300

    Building carteirapro bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = (env, argv) => {
   const config = {
     context: __dirname,
     entry: {
-        'main.bundle': './demo/demo.js',
-        'main.bundle.worker':  'pdfjs-dist/build/pdf.worker.entry',
+      "main.bundle": "./demo/demo.js",
+      "main.bundle.worker": "pdfjs-dist/build/pdf.worker.entry",
+      "main.bundle.pdfdocument": "./lib/pdfDocumentEntry.js",
     },
-    mode: 'none',
+    mode: "none",
     output: {
-      path: path.resolve(__dirname, './demo/bundles'),
-      publicPath: './demo/bundles',
-      filename: '[name].js'
-    }
+      path: path.resolve(__dirname, "./demo/bundles"),
+      publicPath: "./demo/bundles",
+      filename: "[name].js",
+    },
   };
 
   return config;
